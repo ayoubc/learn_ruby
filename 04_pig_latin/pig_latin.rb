bb723aa4542380b6f9442efd85b82ba8c6033a0c
ayoubc <ayoubch807@gmail.com>
2019-12-12 23:43:05 +0100

    04 solution completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,5 +1,21 @@
 #write your code here
-def translate(word)
-    v = ['a', 'e', 'u', 'i', 'o']
-    return word
+def translate(sentence)
+    h = {'a'=>1, 'e'=>1, 'u'=>1, 'i'=>1, 'o'=>1}
+    words = sentence.split(" ")
+    new_words = Array.new
+    words.each do |word|
+        i = 0
+        suffix = ""
+        while h[word[i]] == nil && i < word.length
+            suffix += word[i]
+            i += 1
+            if i < word.length && word[i-1]=='q' && word[i] == 'u'
+                suffix += word[i]
+                i += 1
+            end
+        end
+        new_words.push(word[i..word.length-1] + suffix + "ay")
+    end
+
+    return new_words.join(" ")
 end
