6b790d211a9b12ab2ff3e8b3791187c3ad4c8156
ayoubc <ayoubch807@gmail.com>
2019-12-19 16:47:50 +0100

    all solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+    attr_accessor :title
+
+    def title
+        arr = @title.split(" ")
+        special = {"and"=> 1, "in"=> 1, "the"=>1, "of"=>1, "a"=>1, "an"=>1}
+        arr.each do |word| 
+            if special[word.downcase].nil?
+                word.capitalize!
+            end
+        end
+        arr[0].capitalize!
+        @title = arr.join(" ")
+    end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,34 @@
 class Timer
   #write your code here
+  attr_accessor :seconds,:minutes,:hours
+
+  def initialize
+    @seconds = 0
+    @minutes = 0
+    @hours = 0
+  end
+
+  def time_string
+    arr = Array.new(3, "00")
+    @minutes += @seconds/60
+    @hours += @minutes/60
+    @seconds %= 60
+    @minutes %= 60
+    if @seconds <= 9
+      arr[2] = "0"+@seconds.to_s
+    else
+      arr[2] = @seconds.to_s
+    end
+    if @minutes <= 9
+      arr[1] = "0"+@minutes.to_s
+    else
+      arr[1] = @minutes.to_s
+    end
+    if @hours <= 9
+      arr[0] = "0"+@hours.to_s
+    else
+      arr[0] = @hours.to_s
+    end
+    return arr.join(":")
+  end
 end
